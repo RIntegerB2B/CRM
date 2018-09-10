3f4ca9caad89f238710afa60c92bc9a0f149c2e7
Preveen Chandran <Preveen_Chandran@Dell.com>
2018-09-10 14:04:34 +0530

    Deploy

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -4,6 +4,8 @@ import { NavHeaderComponent } from './shared/nav-header/nav-header.component';
 
 const routes: Routes = [
     { path: 'navheader', component: NavHeaderComponent },
+    { path: '', redirectTo: 'navheader', pathMatch: 'full' },
+    { path: '**', redirectTo: 'navheader', pathMatch: 'full' }
 ];
 
 export const Routing = RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' });
