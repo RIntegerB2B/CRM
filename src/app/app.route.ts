bf91efed5df1db89793787e2505d42d2a05c92fa
Preveen Chandran <Preveen_Chandran@Dell.com>
2018-09-13 14:52:20 +0530

    Path

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,11 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NavHeaderComponent } from './shared/nav-header/nav-header.component';
+import { CustomerManagementComponent } from './customer-management/customer-management/customer-management.component';
 
 
 const routes: Routes = [
-    { path: 'navheader', component: NavHeaderComponent },
-    { path: '', redirectTo: 'navheader', pathMatch: 'full' },
-    { path: '**', redirectTo: 'navheader', pathMatch: 'full' }
+    { path: 'Customer', component: CustomerManagementComponent },
+    { path: '', redirectTo: 'Customer', pathMatch: 'full' },
+    { path: '**', redirectTo: 'Customer', pathMatch: 'full' }
 ];
 
 export const Routing = RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' });
