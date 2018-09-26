18712f45ea73721f7758d9abcc7b42daeed79c8b
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-09-26 15:07:13 +0530

    changes in port

--- deployScript.js
+++ deployScript.js
@@ -1,5 +1,6 @@
 var request = require('request');
-request('http://ec2-35-154-115-74.ap-south-1.compute.amazonaws.com/crm', function (error, response, body) {
+request('http://ec2-35-154-115-74.ap-south-1.compute.amazonaws.com:3020/crm', function (error, response, body) {
   console.log('error:', error);
   console.log('body:', body);
 });
+
