38c87aaa76010c504640e02dcb62cbaa39d4b626
preveen_chandran <Preveen_Chandran@Dell.com>
2018-11-22 12:35:29 +0530

    Server change

--- deployScript.js
+++ deployScript.js
@@ -1,5 +1,5 @@
 var request = require('request');
-request('http://ec2-35-154-115-74.ap-south-1.compute.amazonaws.com:3020/crm', function (error, response, body) {
+request('http://13.126.135.113:3020/crm', function (error, response, body) {
   console.log('error:', error);
   console.log('body:', body);
 });
